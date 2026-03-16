f38335954bdc1e4a09646ea4a03e5afff51c3c02
Abhinav Kumar <abhinavkumar5612@gmail.com>
2026-03-17 04:18:30 +0530

    Story Viewer Completed
    Need to adjust story viewer space.

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from "react"
 import { dummyPostsData } from "../assets/assets"
 import Loading from "../components/Loading"
+import StoriesBar from "../components/StoriesBar"
 
 const Feed = () => {
-  const [feed,setfeeds] = useState([])
-  const [loading,setLoading] = useState(true)
-  const fetchFeeds = async() => {
+  const [feed, setfeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+  const fetchFeeds = async () => {
     setfeeds(dummyPostsData)
+    setLoading(false)
   }
-  useEffect(()=>{
+  useEffect(() => {
     fetchFeeds()
-  },[])
-  return !loading ?(
-    <>
-      <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
-        {/* Stories and Post list  */}
-        <div>
-          <h1>Stories</h1>
-        </div>
-        {/* Right Sidebar  */}
+  }, [])
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+      {/* Stories and Post list  */}
+      <div>
+        <StoriesBar />
         <div className="p-4 space-y-6">
           List of Posts
         </div>
       </div>
-    </>
-  ): <Loading />
+      {/* Right Sidebar  */}
+      <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+        <h1>Recent Messages</h1>
+      </div>
+    </div>
+  ) : <Loading />
 }
 
 export default Feed
